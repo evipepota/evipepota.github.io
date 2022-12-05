20364f32c30cebb461a829b41e5d0e633aa454e0
evipepota <meimuyuta@gmail.com>
2022-12-06 05:43:11 +0900

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="header">
+        <div class="header-container">
+            <div class="header-title">
+                <span class="header-title-text">evip</span>
+            </div>
+            <div class="links-container">
+                <div class="links">
+                    <a href="https://twitter.com/evipepota">Twitter</a>
+                    <a href="https://github.com/evipepota">github</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    background-color: #ffc3ed;
+    padding: 1.5rem;
+    border-radius: 30px;
+}
+
+.header-title {
+    flex-shrink: 0;
+    color: #ffffff;
+    margin-right: 1.5rem;
+}
+
+.header-title-text {
+    font-weight: 600;
+    font-size: 1.5rem;
+    line-height: 2rem;
+    letter-spacing: -0.025em;
+}
+
+.links-container {
+    width: 100%;
+    display: block;
+    flex-grow: 1;
+}
+
+.links a {
+    display: block;
+    color: #ffffff;
+    margin-right: 3rem;
+}
+
+.links a:hover {
+    color: #115e59;
+}
+
+@media (min-width: 1024px) {
+    .links-container {
+        display: flex;
+        align-items: center;
+        width: auto;
+    }
+
+    .links a {
+        display: inline-block;
+    }
+}
+</style>
---
+++ src/components/Tab.vue
@@ -0,0 +1,85 @@
+<script lang="ts">
+export default {
+    data() {
+        return {
+            show: "1",
+        };
+    },
+    methods: {
+        select: function (num: any) {
+            this.show = num;
+        },
+    },
+};
+</script>
+
+
+<template>
+    <div style="width:300px; margin:0 auto;">
+        <ul class="tabnav">
+            <li @click="select('1')" v-bind:class="{ 'active': show == '1' }">
+                <h5>about</h5>
+            </li>
+            <li @click="select('2')" v-bind:class="{ 'active': show == '2' }">
+                <h5>history</h5>
+            </li>
+        </ul>
+    </div>
+    <div v-if="show == '1'" class="tabtext">
+        <br>
+        <h3>好き</h3>
+        <p>
+            Rust <br>
+            ヤドン
+        </p>
+    </div>
+    <div v-else-if="show == '2'" class="tabtext">
+        <br>
+        <p>
+            2021年 4月 静岡大学情報学部入学 <br>
+            2022年 6月 技育博2022参加 <br>
+            2022年 8月 セキュリティキャンプ2022全国大会L2クラス <br>
+            2022年 10月 paizaアルバイト
+        </p>
+    </div>
+</template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.tabtext {
+    max-width: 500px;
+    text-align: center;
+    margin: auto;
+    padding: 10px 30px;
+}
+
+.tabtext p {
+    display: inline-block;
+    text-align: left;
+}
+
+.tabnav {
+    margin: auto;
+    display: flex;
+    list-style-type: none;
+}
+
+.tabnav li {
+    cursor: pointer;
+    width: 50%;
+    background: #ddd;
+    color: #333;
+    padding: 10px;
+    text-decoration: none;
+    border-radius: 10px;
+}
+
+.tabnav li.active {
+    background: #ffc3ed;
+    color: #fff;
+}
+</style>
