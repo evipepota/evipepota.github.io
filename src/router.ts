dc74638f4060c8d0e9c00fd1c8861df3ec39bb69
evipepota <meimuyuta@gmail.com>
2022-12-31 19:49:59 +0900

    refactor: 不要なimportを削除

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,4 @@
 import { createRouter,createWebHistory, RouteRecordRaw, RouterOptions } from 'vue-router';
-import Index from "@/pages/index.vue";
-import Ctf from "@/pages/CTF.vue";
-import Test from "@/pages/test/a.vue";
 import routes from '~pages';
 
 const router = createRouter({
