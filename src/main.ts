237c08578515f501eaed187c9fb108050bb4f1eb
evipepota <meimuyuta@gmail.com>
2022-12-31 19:48:45 +0900

    chore: vuetifyの導入

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,30 @@ import { ViteSSG } from 'vite-ssg'
 import router from './router'
 import generatedRoutes from 'virtual:generated-pages';
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: "mdi",
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+})
+
 const routes = generatedRoutes
 
 export const createApp = ViteSSG(
     App,
     { routes },
-    ({ app, router, routes, isClient, initialState }) => {
+    ({ app }) => {
+        app.use(vuetify)
     },
 )
