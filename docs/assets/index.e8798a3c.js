b5b7d26966dc14d77fe6dc497b8313bdd6d541db
evipepota <shrimpround12@gmail.com>
2024-03-21 17:44:42 +0900

    chore: ignore docs

--- docs/assets/index.e8798a3c.js
+++
@@ -1,1 +0,0 @@
-import{d as t,o,c as s,a as n}from"./app.34a2a810.js";const a={class:"markdown-body"},c=n("h1",null,"sample1",-1),r=[c],i={},l="",f=t({__name:"index",setup(_,{expose:e}){return e({frontmatter:{},excerpt:void 0}),(d,m)=>(o(),s("div",a,r))}});export{f as default,l as excerpt,i as frontmatter};
